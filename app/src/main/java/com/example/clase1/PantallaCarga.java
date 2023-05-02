93575292c6c347555f270db9ec02a0e298a9ed18
LuisT0p20 <luistoribiopalacios@gmail.com>
2023-05-02 10:21:01 -0500

    pantalla de carga y resultados

---
+++ app/src/main/java/com/example/clase1/PantallaCarga.java
@@ -0,0 +1,14 @@
+package com.example.clase1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaCarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_carga);
+    }
+}
