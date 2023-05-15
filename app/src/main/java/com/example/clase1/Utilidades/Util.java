ccd082ce63616bf5de8b97e319bdf6a82e1746ed
LuisT0p20 <luistoribiopalacios@gmail.com>
2023-05-14 21:11:10 -0500

    NUEVO ESTILO REGISTRO

--- app/src/main/java/com/example/clase1/Utilidades/Util.java
+++ app/src/main/java/com/example/clase1/Utilidades/Util.java
@@ -1,7 +1,6 @@
 package com.example.clase1.Utilidades;
 
 public class Util {
-//     public static final String RUTA = "http://192.168.1.84:8080/api/";
-//    /*public static final String RUTA = "http://172.16.145.116:8080/api/";;*/
-     public static final String RUTA = "http://192.168.43.207:8080/api/";
+    /* public static final String RUTA = "http://192.168.1.84:8080/api/";*/
+    public static final String RUTA = "http://172.16.145.116:8080/api/";
 }
