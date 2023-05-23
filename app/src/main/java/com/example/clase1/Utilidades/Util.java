206bb69d1cd4cbe5571fb22e88d9eebaf0b9ee1f
LuisT0p20 <luistoribiopalacios@gmail.com>
2023-05-23 10:57:56 -0500

    nuevos estilos carga-starting-resultados

--- app/src/main/java/com/example/clase1/Utilidades/Util.java
+++ app/src/main/java/com/example/clase1/Utilidades/Util.java
@@ -1,7 +1,7 @@
 package com.example.clase1.Utilidades;
 
 public class Util {
-public static final String RUTA = "http://192.168.1.84:8080/api/";
+public static final String RUTA = "http://192.168.100.78:8080/api/";
 //    public static final String RUTA = "http://172.16.145.116:8080/api/";
 //    public static final String RUTA = "http://172.16.165.55:8080/api/";
 }
